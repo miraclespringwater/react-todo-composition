6c56ec1022ff4c153f6d0c8fe5d32e90d911b25f
George Colletti Barajas <miracle@dreamsmith.net>
2023-04-01 21:59:36 -0500

    initial very simple todo app

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,75 @@
+import { useState } from "react";
+
+const genId = () => Math.round(Math.random() * 9999);
+
 const App = () => {
-  return <h1>Hello World!</h1>;
-}
+  const [input, setInput] = useState("");
+  const [tasks, setTasks] = useState([]);
+
+  const createTask = (title) => {
+    setTasks((oldTasks) => [
+      ...oldTasks,
+      { id: genId(), title, completed: false },
+    ]);
+  };
+
+  const deleteTaskById = (id) => {
+    setTasks((oldTasks) =>
+      oldTasks.filter((task) => {
+        return task.id !== id;
+      })
+    );
+  };
+
+  const completeTaskById = (id) => {
+    setTasks((oldTasks) =>
+      oldTasks.map((task) => {
+        return (task.id === id && { ...task, completed: true }) || task;
+      })
+    );
+  };
+
+  const handleChange = (event) => {
+    setInput(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    createTask(input);
+    setInput("");
+  };
+
+  return (
+    <div className="app">
+      <div>To-Do App</div>
+      <form onSubmit={handleSubmit}>
+        <input type="text" onChange={handleChange} value={input} />
+      </form>
+      <ul>
+        {tasks.map((task, index) => {
+          return (
+            <li key={index}>
+              <span
+                style={
+                  (task.completed && { textDecoration: "line-through" }) || null
+                }
+              >
+                {task.title}
+              </span>
+              {!task.completed && (
+                <button onClick={() => completeTaskById(task.id)}>
+                  Complete Task
+                </button>
+              )}
+              <button onClick={() => deleteTaskById(task.id)}>
+                Delete Task
+              </button>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
 
 export default App;
